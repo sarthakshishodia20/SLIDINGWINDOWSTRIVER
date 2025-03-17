d692406e2b84f5fb092ae28c988be7c21d8b550e
SARTHAK SHISHODIA <122453870+sarthakshishodia20@users.noreply.github.com>
2025-03-17 16:44:56 +0530

    Create MAXIMUMCARDPOINTSFRMLEFTSUMANDRIGHTSUMEXCEPTMIDDLE.java

---
+++ MAXIMUMCARDPOINTSFRMLEFTSUMANDRIGHTSUMEXCEPTMIDDLE.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxScore(int[] arr, int k) {
+        int maxSum=0;
+        int lSum=0;
+        int rSum=0;
+        int n=arr.length;
+        for(int i=0;i<k;i++){
+            lSum+=arr[i];
+        }
+        maxSum=lSum;
+        int rIndex=n-1;
+        for(int i=k-1;i>=0;i--){
+            lSum=lSum-arr[i];
+            rSum+=arr[rIndex];
+            maxSum=Math.max(lSum,rSum);
+            rIndex--;
+        }
+        return maxSum;
+    }
+}
